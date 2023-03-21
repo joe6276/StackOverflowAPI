5062f8334293c7cb277d4cee8e0445aecf66cc6f
Jonathan Ndambuki <joendambuki16@gmail.com>
2023-03-21 18:46:31 +0300

    worked on Forgot password and sent  an Email with instructions

---
+++ StackOverflowAPI/EmailServices/ForgotPassword.cs
@@ -0,0 +1,76 @@
+﻿namespace StackOverflowAPI.EmailServices
+{
+    public class ForgotPassword
+    {
+        private string template { get; set; }
+
+        public ForgotPassword (string Name , string message)
+        {
+            template =$@"
+<!DOCTYPE html>
+<html lang=""en"">
+<head>
+    <meta charset=""UTF-8"">
+    <meta name=""viewport"" content=""width=device-width, initial-scale=1.0"">
+    <title>Document</title>
+</head>
+
+<body>
+    <table class=""container"">
+      <tr>
+          <td>
+              <table width=""200"" style=""margin: auto;"">
+                 <tr>
+                     <td style=""background-color: white; align:center;"">
+                        <p style=""color: #000000;font-size:45px; font-weight:bold"">
+                            StackOverflow Lite</p>
+                     </td>
+                 </tr>
+               </table>
+          </td>
+      </tr>
+      <tr>
+          <td style=""padding: 20px;"">
+            <table width=""600"" style=""margin: auto;"">
+                <tr>
+                    <td>
+                        
+                      
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <p> Hello {Name} Wlecome to stackOverflowLite</p></h2>
+                        <p style=""color: black;font-size: 30px; margin: 10px 0px;font-weight: 400;"">
+                            Follow the Following  Verify your Account
+                           </p>
+                        <a style=""color: black;font-size: 20px;Margin: 10px 0px;font-weight: 400; max-width: 500px;"" href=""https://localhost:7247/api/Auth/login/{message}"">
+                                Verify
+                            </a>
+                       
+                        
+                    </td>
+                </tr>
+            </table>
+        </td>
+      </tr>
+      <tr>
+          <td>
+              <table width=""600"" style=""margin: auto;"">
+                
+            </table>
+          </td>
+      </tr>
+    </table>
+</body>
+</html>
+";
+        }
+
+       public string getTemplate()
+        {
+            return template;    
+        }
+
+    }
+}
---
+++ StackOverflowAPI/Migrations/20230321135619_addedForgot.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StackOverflowAPI.Migrations
+{
+    public partial class addedForgot : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PasswordResetToken",
+                table: "users",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ResetTokenExpires",
+                table: "users",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "VerificationToken",
+                table: "users",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "VerifiedAt",
+                table: "users",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PasswordResetToken",
+                table: "users");
+
+            migrationBuilder.DropColumn(
+                name: "ResetTokenExpires",
+                table: "users");
+
+            migrationBuilder.DropColumn(
+                name: "VerificationToken",
+                table: "users");
+
+            migrationBuilder.DropColumn(
+                name: "VerifiedAt",
+                table: "users");
+        }
+    }
+}
